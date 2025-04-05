e071fadb85dfc838cc7549babbde6691104f1273
ishaan <ishaanbhalla1998@gmail.com>
2025-04-05 09:09:22 -0700

    chore: setup `nativewind` with expo

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function HomeScreen() {
   return (
     <View>
-      <Text>Home</Text>
+      <Text className="font-bold text-3xl">Home</Text>
     </View>
   );
 }
